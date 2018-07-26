f5ac82c5fbff2c08edd79d25f82b7adfa6160cb0
Johnny Soong <johnnysoong@hotmail.com>
2018-07-26 18:58:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let katzDeliLine = [];
 
 function takeANumber(name) {
   katzDeliLine.push(name);
-  return `Welcome, ${name}.  You are number ${katzDeliLine.length}`;
+  return `Welcome, ${name}.  You are number ${katzDeliLine.length} in line.`;
 }
 
 function currentLine(katzDeliLine) {
