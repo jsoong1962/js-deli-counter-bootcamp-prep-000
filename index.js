2d1b48c901759b98079bfa09d596715fadc0e03a
Johnny Soong <johnnysoong@hotmail.com>
2018-07-26 18:52:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ function takeANumber(name) {
 }
 
 function currentLine(katzDeliLine) {
-  
+  let string = 'The line is currently: '; 
+  katzDeliLine.forEach((element, i) => string += `${i+1}: ${katzDeliLine[i]}, `);
+  return string.slice(0, -2);
 }
 
-const currentLine = (katzDeliLine) => { 
-
-};
+function nowServing() {
+  return Currently
+}
 
 const nowServing = () => {`Currently server ${katzDeliLine.shift()}`};
 
