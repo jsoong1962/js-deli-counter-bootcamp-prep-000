24be7c8602cb0421fa5d6ba2a888a9f51321bba7
Johnny Soong <johnnysoong@hotmail.com>
2018-07-26 18:47:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let katzDeliLine = [];
 
-const takeANumber = name => {katzDeliLine.push(name);
+const takeANumber = (name) => {katzDeliLine.push(name);
 return `Welcome, ${name}.  You are number ${katzDeliLine.length} in line.`;};
 
-const currentLine = katzDeliLine => {let string = 'The line is currently: '; 
+const currentLine = (katzDeliLine) => {let string = 'The line is currently: '; 
 katzDeliLine.forEach((element, i) => string += `${i+1}: ${katzDeliLine[i]}, `);
 return string.slice(0, -2);
 };
